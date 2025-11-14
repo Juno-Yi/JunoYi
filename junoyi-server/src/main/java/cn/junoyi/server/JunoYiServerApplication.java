b6842393e4ed5df81ab389cdc4e1ba476b050496
EatFan <eatfan0921@163.com>
2025-11-14 20:21:37 +0800

    init: init project

---
+++ junoyi-server/src/main/java/cn/junoyi/server/JunoYiServerApplication.java
@@ -0,0 +1,12 @@
+package cn.junoyi.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunoYiServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JunoYiServerApplication.class);
+    }
+}
