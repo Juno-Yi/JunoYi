4be010cee97b93a2c41cbadcc04e2412fc4ee3d1
EatFan <eatfan0921@163.com>
2025-12-15 14:14:55 +0800

    refactor(event): 重构事件监听器实现方式
    - 将 Test2EventListener 类从使用 @EventListener 注解改为实现 Listener 接口
    - 更新事件处理方法参数类型为 SpringApplicationStartingEvent
    - 移除不再使用的 TestEvent 导入
    - 调整事件处理逻辑以适应新的事件类型
    - 移除已弃用的 EventPriority 枚举引用
    - 更新事件处理方法名称和输出内容

--- junoyi-module/junoyi-module-demo/src/main/java/com/junoyi/demo/listener/Test2EventListener.java
+++ junoyi-module/junoyi-module-demo/src/main/java/com/junoyi/demo/listener/Test2EventListener.java
@@ -1,15 +1,17 @@
 package com.junoyi.demo.listener;
 
-import com.junoyi.demo.event.TestEvent;
 import com.junoyi.framework.event.annotation.EventHandler;
-import com.junoyi.framework.event.annotation.EventListener;
-import com.junoyi.framework.event.enums.EventPriority;
+import com.junoyi.framework.event.core.Listener;
+import com.junoyi.framework.event.domain.spring.SpringApplicationStartingEvent;
 
-@EventListener
-public class Test2EventListener {
+public class Test2EventListener implements Listener {
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onTestEven32t(TestEvent event){
-        System.out.println("优先级 最高 321321321");
+    @EventHandler
+    public void onTestEven32t(SpringApplicationStartingEvent event){
+        System.out.println("===============");
+        System.out.println(" 测试  SpringApplicationStartingEvent 事件监听");
+        System.out.println("===============");
     }
+
+
 }
