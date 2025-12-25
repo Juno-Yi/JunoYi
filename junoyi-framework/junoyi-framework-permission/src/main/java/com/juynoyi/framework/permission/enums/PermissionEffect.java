2fcd3d17cae4643e8193df53440cafaf99184853
EatFan <eatfan0921@163.com>
2025-12-25 17:39:21 +0800

    feat(permission): 添加权限效果枚举类
    - 定义了权限控制中可能的效果类型
    - 包含允许效果(ALLOW)和拒绝效果(DENY)两种状态
    - 用于表示某个权限操作是被允许还是被拒绝
    - 提供了权限系统的基础枚举定义

---
+++ junoyi-framework/junoyi-framework-permission/src/main/java/com/juynoyi/framework/permission/enums/PermissionEffect.java
@@ -0,0 +1,20 @@
+package com.juynoyi.framework.permission.enums;
+
+
+/**
+ * 权限效果枚举类
+ * 定义了权限控制中可能的效果类型，用于表示某个权限操作是被允许还是被拒绝
+ *
+ * @author Fan
+ */
+public enum PermissionEffect {
+    /**
+     * 允许效果 - 表示权限操作被允许执行
+     */
+    ALLOW,
+    /**
+     * 拒绝效果 - 表示权限操作被拒绝执行
+     */
+    DENY
+}
+
