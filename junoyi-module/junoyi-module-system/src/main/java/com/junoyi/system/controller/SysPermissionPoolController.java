64be06f1b80d7ee6e8e7f236133528ffa225e545
EatFan <eatfan0921@163.com>
2026-01-06 18:51:22 +0800

    docs(controller): 添加系统权限池控制器API文档
    - 为SysPermissionPoolController类添加详细的JavaDoc注释
    - 说明控制器提供系统权限池相关的REST API接口功能
    - 描述权限统一管理和分配的业务用途
    - 添加作者信息标识代码维护责任人

--- junoyi-module/junoyi-module-system/src/main/java/com/junoyi/system/controller/SysPermissionPoolController.java
+++ junoyi-module/junoyi-module-system/src/main/java/com/junoyi/system/controller/SysPermissionPoolController.java
@@ -4,6 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 系统权限池控制器
+ * 提供系统权限池相关的REST API接口，用于权限的统一管理和分配
+ *
+ * @author Fan
+ */
 @RestController
 @RequestMapping("/system/permission-pool")
 @RequiredArgsConstructor
