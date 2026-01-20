8fbde38135cb870d3cc78fd5501c239a3078d97c
EatFan <eatfan0921@163.com>
2026-01-20 20:47:14 +0800

    feat(file): 实现文件类型自动检测功能
    - 添加了 FileTypeDetector 工具类用于检测文件业务类型
    - 集成 hutool 库进行文件操作和字符串处理
    - 实现上传文件时自动根据文件类型检测存储目录
    - 当未指定存储路径时自动检测文件类型并确定业务类型
    - 重构上传方法以支持自动类型检测逻辑

--- junoyi-framework/junoyi-framework-file/src/main/java/com/junoyi/framework/file/helper/FileHelper.java
+++ junoyi-framework/junoyi-framework-file/src/main/java/com/junoyi/framework/file/helper/FileHelper.java
@@ -1,11 +1,14 @@
 package com.junoyi.framework.file.helper;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
 import com.junoyi.framework.file.domain.FileInfo;
 import com.junoyi.framework.file.enums.FileBusinessType;
 import com.junoyi.framework.file.factory.FileStorageFactory;
 import com.junoyi.framework.file.storage.FileStorage;
 import com.junoyi.framework.file.strategy.UploadStrategy;
 import com.junoyi.framework.file.strategy.UploadStrategyFactory;
+import com.junoyi.framework.file.util.FileTypeDetector;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,22 +28,34 @@ public class FileHelper {
 
     /**
      * 上传文件（使用默认存储）
+     * 自动根据文件类型检测存储目录
      *
      * @param file 文件
      * @return 文件信息
      */
     public FileInfo upload(MultipartFile file) {
-        return upload(file, null);
+        // 根据文件类型自动检测业务类型
+        String contentType = file.getContentType();
+        String extension = FileUtil.extName(file.getOriginalFilename());
+        String businessType = FileTypeDetector.detectBusinessType(contentType, extension);
+        
+        return upload(file, businessType);
     }
 
     /**
      * 上传文件到指定路径
      *
      * @param file 文件
-     * @param path 存储路径
+     * @param path 存储路径（业务类型）
      * @return 文件信息
      */
     public FileInfo upload(MultipartFile file, String path) {
+        // 如果没有指定路径，自动检测文件类型
+        if (StrUtil.isBlank(path)) {
+            String contentType = file.getContentType();
+            String extension = FileUtil.extName(file.getOriginalFilename());
+            path = FileTypeDetector.detectBusinessType(contentType, extension);
+        }
         FileStorage storage = fileStorageFactory.getStorage();
         return storage.upload(file, path);
     }
@@ -79,7 +94,7 @@ public class FileHelper {
     /**
      * 上传文件流
      *
-     * @param inputStream 输入流
+     * @param inputStream 输入流  
      * @param fileName    文件名
      * @param path        存储路径
      * @param contentType 文件类型
